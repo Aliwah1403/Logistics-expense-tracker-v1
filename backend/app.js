dd7f0a60e23c0369437305f445121cdddd4cf97e
Curtis Aliwah <aliwahcurtis5@gmail.com>
2024-02-28 00:55:21 +0300

    Installed the firebase-admin dependency and initialised the admin

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const admin = require("firebase-admin");
+const serviceAccount = require("./config/logistics-expense-tracker-firebase-adminsdk-r0h7n-d0b19ca46d.json");
+
+// firebase admin
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+let db = admin.firestore()
 
 const app = express();
 
@@ -17,7 +26,5 @@ const server = () => {
     console.log(`Listening on port ${PORT}`);
   });
 };
-
-
 
 server();
