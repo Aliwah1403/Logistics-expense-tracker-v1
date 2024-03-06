aa7d1a315d8b1ba26cd084fe70a1d8c82dd85601
Curtis Aliwah <aliwahcurtis5@gmail.com>
2024-03-06 15:19:36 +0300

    Removed everything from truckModel and truckController to allow me to switch to using mongoDB as my database

--- backend/routes/trucks.js
+++ backend/routes/trucks.js
@@ -1,7 +1,7 @@
+const { addTruck } = require("../controllers/truckController");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World Route works");
-});
+router.post("/add-truck", addTruck);
 
 module.exports = router;
