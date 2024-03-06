46a6ebdfdf8f565c5a91e24a8e8317cbe3a9c172
Curtis Aliwah <aliwahcurtis5@gmail.com>
2024-03-06 15:55:53 +0300

    Installed mongoose package and connected byapp to mongoDB

--- backend/routes/trucks.js
+++ backend/routes/trucks.js
@@ -2,6 +2,6 @@ const { addTruck } = require("../controllers/truckController");
 
 const router = require("express").Router();
 
-router.post("/add-truck", addTruck);
+// router.post("/add-truck", addTruck);
 
 module.exports = router;
